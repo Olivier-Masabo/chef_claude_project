279f7720eebd480e9b8405f334be5ffee0d7f2c7
Olivier-Masabo <masaboolivier21@gmail.com>
2026-02-17 18:42:42 +0200

    adding simple functionality in main section

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
         <div className="flex items-center justify-center gap-3 mt-5">
             <img src="/OIP.webp" alt="chef image"  className="w-16 "/>
             <h2 className="font-semibold text-3xl">Chef Claude</h2>
-            <hr  />
+            <hr />
         </div>
     
        
--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,12 +1,39 @@
+import { useState } from "react"
+
  export default function Main(){
+   const[ingredients, setIngredients] =useState([])
+
+   const ingredientListItems = ingredients.map(ingredient =>(
+      <li className="list-disc" key={ingredient}>{ingredient}</li>
+   ))
+
+   function addIngredients(formData){
+   
+      const newIngredients = formData.get("ingredient")
+      setIngredients(prevInput =>[...prevInput,newIngredients])
+   }
 
     return(
        
-       <div>
-        <form className="flex flex-wrap items-center justify-center mt-8" name="">
-            <input type="text" className="border p-2 rounded-lg w-[20%] outline-0 border-gray-400 shadow-gray-400 shadow-md" placeholder="E.g: fish" />
-            <button className="border-2 p-2 rounded-lg text-white bg-black cursor-pointer">+ Add ingredient</button>
+       <main>
+        <form 
+        action={addIngredients}
+        className="flex flex-wrap items-center justify-center mt-8" 
+      >
+
+            <input
+             type="text" 
+             className="border p-2 rounded-lg w-[25%] outline-0 border-gray-400 shadow-gray-400 shadow-md" 
+             placeholder="E.g: fish"
+             name="ingredient" />
+
+            <button 
+            className="border-2 p-2 rounded-lg text-white bg-black cursor-pointer sm:text-sm">+ Add ingredient</button>
         </form>
-       </div>
+
+        <ul className="w-[30%] mx-auto">
+         {ingredientListItems}
+        </ul>
+       </main>
     )
  }
