f16e71c4e47755cb10db841aa16bbad31e6da515
Olivier-Masabo <masaboolivier21@gmail.com>
2026-02-19 21:56:18 +0200

    formatting response from api

--- src/component/ClaudeRecipe.jsx
+++ src/component/ClaudeRecipe.jsx
@@ -1,1 +1,10 @@
- 
+ import ReactMarkdown from "react-markdown"
+ export default function ClaudeRecipe(props){
+
+    return (
+       <section>
+         <h2>Chef Claude Recommends:</h2>
+         <ReactMarkdown>{props.recipe}</ReactMarkdown>
+         </section>
+    )
+ }
